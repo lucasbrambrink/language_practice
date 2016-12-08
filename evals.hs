8a3bcd119fefa026cd4790750235f4975c68bead
lucasbrambrink <lbrambrink@gmail.com>
2016-12-08 01:19:50 -0500

    removes unnecessary files

--- evals.hs
+++
@@ -1,8 +0,0 @@
-import System.Environment (getArgs)
-
-main = do
-	[inpFile] <- getArgs
-	input <- readFile inpFile
-	-- print your output to stdout
-	maps x = if null x then [] else (head x):(maps (tail x))
-	x <- mapM_ putStrLn $ lines input
